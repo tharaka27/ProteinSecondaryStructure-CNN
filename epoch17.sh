085fe862dc2ae5083435996a13e81bd1c09a33b8
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2022-06-15 01:26:11 +0530

    removed update upgrade

--- epoch17.sh
+++ epoch17.sh
@@ -5,25 +5,25 @@ echo "Setting background"
 #sudo apt update
 #sudo apt upgrade
 echo "Installing python-pip"
-#sudo apt-get install python3-pip python-dev
+sudo apt-get install python3-pip python-dev
 echo "Installing requirements.txt"
-#pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
 cd dataset
 
-#curl http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_6133.npy.gz --output cullpdb+profile_6133.npy.gz 
+curl http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_6133.npy.gz --output cullpdb+profile_6133.npy.gz 
 
-#gunzip cullpdb+profile_6133.npy.gz
+gunzip cullpdb+profile_6133.npy.gz
 
-#wget http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_6133_filtered.npy.gz
+wget http://www.princeton.edu/~jzthree/datasets/ICML2014/cullpdb+profile_6133_filtered.npy.gz
 
-#gunzip cullpdb+profile_6133_filtered.npy.gz
+gunzip cullpdb+profile_6133_filtered.npy.gz
 
-#wget http://www.princeton.edu/~jzthree/datasets/ICML2014/cb513+profile_split1.npy.gz
+wget http://www.princeton.edu/~jzthree/datasets/ICML2014/cb513+profile_split1.npy.gz
 
-#gunzip cb513+profile_split1.npy.gz
+gunzip cb513+profile_split1.npy.gz
 
-#cd ..
+cd ..
 
 
 echo "Set environment variables"
@@ -36,7 +36,7 @@ export CNN_WIDTH=13
 #python3 main.py
 echo "--------------------------------------------------------------"
 #export CNN_WIDTH=15
-python3 main.py
+#python3 main.py
 echo "--------------------------------------------------------------"
 export CNN_WIDTH=17
 python3 main.py
